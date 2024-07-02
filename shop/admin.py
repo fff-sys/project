87acb93a5f8f817bf70773d1681613f36e5237b5
Lenar <windus08102009@gmail.com>
2024-07-02 18:29:45 +0300

    .

---
+++ shop/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import  Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+# Register your models here.
+admin.site.register(Product, ProductAdmin)
